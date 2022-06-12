2601aca69cff2d260fa3a5d0e23304b71cc7b9f0
Chris Ko <ko_ka_chun@hotmail.com>
2022-06-12 19:01:47 +0100

    complete the second challenge.

--- js-challenges/index.js
+++ js-challenges/index.js
@@ -1,4 +1,4 @@
-// Challenges
+// JS Challenges
 
 // 1. Write a function that takes a number and returns true if it is a positive number 
 // and false if it is a negative number.
@@ -9,6 +9,18 @@ function isNumberPositive(number) {
     return false;
 }
 
-console.log(isNumberPositive(-1));
-console.log(isNumberPositive(10));
-console.log(isNumberPositive(0));  
+console.log(isNumberPositive(-1)); // returns false
+console.log(isNumberPositive(10)); // returns true
+console.log(isNumberPositive(0)); // returns false
+
+
+// 2. Write a function that takes a number of days and converts it into an age.
+function convertDaysToAge(numberOfDays) {
+    const daysInOneYear = 365;
+    let toAge = numberOfDays / daysInOneYear;
+    return toAge;
+}
+
+console.log(convertDaysToAge(548)); 
+console.log(convertDaysToAge(3650)); // returns 10
+console.log(convertDaysToAge(6570)); // returns 18
